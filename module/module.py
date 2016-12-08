6c21a2acc332c462497128d473d7e475e6dfcbab
urluba <lucas.barriere@gmail.com>
2016-12-08 23:37:59 +0000

    FIX	fix AWS connection NEW	Toying around to match my setup

--- module/module.py
+++ module/module.py
@@ -82,7 +82,7 @@ class AWS_importer_arbiter(BaseModule):
     def init(self):
         logger.debug("[AWS Importer Module]: Try to open a AWS connection")
         for region in self.regions:
-            self.cons.append(get_driver(getattr(Provider, region.upper()))(self.api_key, self.secret))
+            self.cons.append(get_driver(getattr(Provider, "EC2"))(self.api_key, self.secret, region=region.lower()))
 
         logger.info("[AWS Importer Module]: Connection opened")
 
@@ -105,62 +105,105 @@ class AWS_importer_arbiter(BaseModule):
         for n in nodes:
             h = {}
 
+            hostgroups = []
+
             # The templates we will use to really configure the VM
             tags = []
             if self.default_template:
                 tags.append(self.default_template)
+
             tags.append('EC2')
+
             # Append the instance id to the name since AWS allows for Name to be duplicated across instances
             h['host_name'] = unicode(n.name + "_" + n.id)
             
+            logger.info("[AWS Importer Module]: I see %s" %  h['host_name'])
+
             # Now the network part, try to get some :)
             try:
                 h['_EC2_PRIVATE_IP'] = unicode(n.private_ips[0])
             except IndexError:
                 h['_EC2_PRIVATE_IP'] = u''
+            logger.debug("[AWS Importer Module]: Private IP %s" % h['_EC2_PRIVATE_IP'])
+
             try:
                 h['_EC2_PUBLIC_IP'] = unicode(n.public_ips[0])
             except IndexError:
                 h['_EC2_PUBLIC_IP'] = u''
             
-            # use public ip, else fall back to private one
-            if h['_EC2_PUBLIC_IP']:
-                h['address'] = h['_EC2_PUBLIC_IP']
-            elif h['_EC2_PRIVATE_IP']:
+            # use private ip
+            if h['_EC2_PRIVATE_IP']:
                 h['address'] = h['_EC2_PRIVATE_IP']
+            else:
+                logger.warning("[AWS Importer Module]: host %s has no private IP" % h['host_name'])
+                h['address'] = u''
+                h['initial_state'] = u'u'
+                h['active_checks_enabled'] = u'0'
+                h['passive_checks_enabled'] = u'0'
+
+            logger.debug("[AWS Importer Module]: Shinken IP %s" % h['address'])
 
             # Ok massive macro setup, but if possible in a clean way
             for (k, v) in n.extra.iteritems():
+
                 prop = '_EC2_'+k.upper()
+
                 if isinstance(v, list):
                     try:
+                        logger.debug("[AWS Importer Module]: list %s" % k)
                         h[prop] = ','.join(filter(None,v))
                     except TypeError:
                         logger.debug(k + " is not a simple list: " + str(v))
                 elif isinstance(v, dict):
+                    logger.debug("[AWS Importer Module]: dict %s" % k)
                     h[prop] = ','.join(['%s:%s' % (i, j) for (i,j) in v.iteritems()])
                 else:
+                    logger.debug("[AWS Importer Module]: %s = %s" % (k, v))
                     h[prop] = unicode(v)
+
                 # Special hooks
+                if k == 'tags' and 'aws:cloudformation:stack-name' in v:
+                    logger.info( "[AWS Importer Module]: adding %s to group %s" % (h['host_name'], v['aws:cloudformation:stack-name']))
+                    hostgroups.append(v['aws:cloudformation:stack-name'])
+
+                if k == 'tags' and 'Role' in v:
+                    logger.info( "[AWS Importer Module]: adding %s to group %s" % (h['host_name'], v['Role']))
+                    hostgroups.append(v['Role'])
+
                 # We take the "use" tag as a use parameter
-                if k == 'tags' and 'use' in v:
-                    if v['use'] == self.ignore_tag:
-                        break
-                    else:
-                        tags.append(v['use'])
+                #if k == 'tags' and 'use' in v:
+                    #if v['use'] == self.ignore_tag:
+                        #break
+                    #else:
+                        #tags.append(v['use'])
                 # Also put as template the instance type
-                if k == 'instancetype':
-                    tags.append(v)
+                #if k == 'instancetype':
+                    #tags.append(v)
             else:
+                # Non running hosts are set down
+                if h.get('_EC2_STATUS') != "running":
+                    logger.warning("[AWS Importer Module]: %s is down" % h['host_name'] )
+                    h['initial_state'] = u'u'
+                    h['active_checks_enabled'] = u'0'
+                    h['passive_checks_enabled'] = u'0'
+
                 # The tag order is not the good, precise data are on the end, we want them
                 # first
                 tags.reverse()
+
                 h['use'] = ','.join(tags)
+                h['hostgroups'] = ','.join(hostgroups)
+
                 if self.poller_tag == 'availabilityzone':
                     h['poller_tag'] = h['_EC2_AVAILABILITY']
                 elif self.poller_tag == 'region':
-                    h['poller_tag'] = h['_EC2_AVAILABILITY'][:-1].replace('-','_').upper()
+                    h['poller_tag'] = h['_EC2_AVAILABILITY'][:-1]
+                    #h['poller_tag'] = h['_EC2_AVAILABILITY'][:-1].replace('-','_').upper()
+
+		#logger.info("[AWS Importer Module]: %s" % h)
                 hosts.append(h)
+
+        # End for n in nodes:
 
         print "Discovered hosts"
         pprint(hosts)
